c4a4a75771ec495794a2bb919517c949db6887a4
cesar <cesaroliva.work@gmail.com>
2025-09-26 22:36:52 -0600

    Arreglando hover

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click', () => {
+        if(window.innerWidth > 768) return;
         const list = card.querySelector('.list');
         const img = card.querySelector('img');
         if (getComputedStyle(list).opacity === '0') {
@@ -9,5 +10,15 @@ document.querySelectorAll('.card').forEach(card => {
             list.style.opacity = '0';
             img.style.opacity = '1';
         }
+
+        setTimeout(() => {
+            if (getComputedStyle(list).opacity === '1') {
+                list.style.opacity = '0';
+                img.style.opacity = '1';
+            } else {
+                list.style.opacity = '1';
+                img.style.opacity = '0';
+            }
+        },4000);
     });
 });
